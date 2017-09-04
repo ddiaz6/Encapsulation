1edddc8571738764b1d420b2b2ae61c27e144a39
HP <HP@10.30.12.75>
2017-09-04 16:16:10 -0500

    Added methods to the organization to help with a new HR employee. Also added a method to compare a certain SSN to an employee.

--- src/lab4/HumanResourcesPerson.java
+++ src/lab4/HumanResourcesPerson.java
@@ -14,15 +14,22 @@ import java.util.ArrayList;
 public class HumanResourcesPerson {
     private ArrayList<Employee> employees;
 
+    public HumanResourcesPerson() {
+        employees = new ArrayList<>();
+    }
+
     public void hireEmployee(String firstName, String lastName, String ssn, String cubeId){
         Employee newEmployee = new Employee(firstName,lastName,ssn);
         newEmployee.doFirstTimeOrientation(cubeId);
         employees.add(newEmployee);
     }
     
-    public void getEmployeeStatuses(){
+    public void getStatusOfEmployee(String ssn){
         for(Employee e : employees){
-            e.getReportService().outputReport();
+          if(e.getSsn().equals(ssn) ){
+              e.getReportService().outputReport();
+              return;
+          }
         }
     }
     
--- src/lab4/Organization.java
+++ src/lab4/Organization.java
@@ -10,6 +10,27 @@ package lab4;
  * @author HP
  */
 public class Organization {
+    private HumanResourcesPerson humanResourcesPerson;
+
+    public Organization() {
+        this.humanResourcesPerson = new HumanResourcesPerson();
+    }
+      
+    public void hireEmployee(String firstName, String lastName, String ssn, String cubeId){
+        humanResourcesPerson.hireEmployee(firstName, lastName, ssn, cubeId);
+        humanResourcesPerson.getStatusOfEmployee(ssn);
+    }
+        
+    public HumanResourcesPerson getHumanResourcesPerson() {
+        return humanResourcesPerson;
+    }
+
+    public void setHumanResourcesPerson(HumanResourcesPerson humanResourcesPerson) {
+        this.humanResourcesPerson = humanResourcesPerson;
+    }
+    
+    
+        
     
     
 }
